e857d82ad185a23a05b41f7a88b4b33e4883f150
Jhenbert Villamucho <jvv205756@gmail.com>
2023-01-26 21:46:59 +0800

    commited in homepage

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,14 @@
 import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+// import * as bootstrap from 'bootstrap'
 
 // Import just what we need - for optimization
 
 // import 'bootstrap/js/dist/alert';
 // import "bootstrap/js/dist/button";
 // import 'bootstrap/js/dist/carousel';
-// import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/collapse';
 // import "bootstrap/js/dist/dropdown";
 // import 'bootstrap/js/dist/modal';
 // import 'bootstrap/js/dist/offcanvas';
