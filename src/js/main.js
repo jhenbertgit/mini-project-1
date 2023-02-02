2742aa30f8431d10a34757d4e756f3dd2ddfc394
Jhenbert Villamucho <jvv205756@gmail.com>
2023-02-02 22:47:07 +0800

    added symptoms, prevention, and faqs html

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ import "../scss/styles.scss";
 
 // import 'bootstrap/js/dist/alert';
 // import "bootstrap/js/dist/button";
-// import 'bootstrap/js/dist/carousel';
+import 'bootstrap/js/dist/carousel';
 import "bootstrap/js/dist/collapse";
 // import "bootstrap/js/dist/dropdown";
 import "bootstrap/js/dist/modal";
